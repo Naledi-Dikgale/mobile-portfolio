a6abbaad18d939df57c57b8f7bef67b9cbfd60d9
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-07 17:57:48 +0300

    Make popup-card using javascript

--- script.js
+++ script.js
@@ -28,3 +28,57 @@ about.onclick = function () {
 contact.onclick = function () {
   hamburgerMenu.style.display = "none";
 }
+
+// --- popup-window ----
+
+const cardInfo = {
+  imageURL: "./assets/tonic.png",
+  name: "Tonic",
+  company: "CANOPY",
+  role: "Back End Dev",
+  year: "2015",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required",
+  tech: ["html", "css", "javascript"],
+  projectURL: "",
+  firstBtnId: "sourceBtn",
+  secondBtnId: "demoBtn"
+};
+
+const modalContainer = document.querySelector('.modal-container');
+const popupCard = document.querySelector('.cards-modal');
+
+const firstCardBtn = document.querySelector('#first-card');
+
+function popup() {
+  modalContainer.style.display = 'block';
+  popupCard.innerHTML = `
+  <h2 class="h-text-name">${cardInfo.name}</h2>
+
+  <img src="${cardInfo.imageURL}" alt="Desktop Project Live Preview" class="project-preview">
+
+  <ul class="skills-list">
+    <li class="checkbox"><span class="info-text">${cardInfo.infoText}</span></li>
+    <li class="checkbox"><i class="ri-checkbox-blank-circle-fill dots"></i>
+      <span class="info-text">${cardInfo.role}</span>
+    </li>
+    <li class="checkbox"> <i class="ri-checkbox-blank-circle-fill dots"></i>
+      <span class="info-text">${cardInfo.year}</span>
+    </li>
+  </ul>
+
+  <div class="project-info">
+    <p class="project-description">${cardInfo.description}</p>
+
+    <ul class="tech-list">
+      <li class="project-tech">${cardInfo.tech[0]}</li>
+      <li class="project-tech">${cardInfo.tech[1]}</li>
+      <li class="project-tech">${cardInfo.tech[2]}</li>
+    </ul>
+    <button class="outlined-button ${cardInfo.firstBtnId}">See Live</button>
+    <button class="outlined-button ${cardInfo.secondBtnId}">See Source</button>
+
+  </div>
+`;
+}
+
+firstCardBtn.addEventListener('click', popup);
