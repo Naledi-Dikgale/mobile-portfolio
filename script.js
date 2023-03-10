51fd880ac9733d662131450ca9dcb181831e8e99
naledi-dikgale <naledi22khosi@gmail.com>
2023-03-10 11:58:57 +0200

    Add script file for form validation

--- script.js
+++ script.js
@@ -358,3 +358,26 @@ desktopProjects.forEach(element => {
       }
   }
 })
+
+//form validation starts
+
+const contactForm = document.querySelector('.contact-form');
+const contactError = document.querySelector('.contact-error');
+const emailInput = document.querySelector('#email');
+
+
+  function submitForm(event){
+  if (emailInput.value !== emailInput.value.toLowerCase()){
+    contactError.style.display = 'block';
+    contactError.style.backgroundColor = 'red';
+    contactError.textContent = "Email address should be lowercase";
+    event.preventDefault();
+
+  }
+
+  // else {
+  //   contactError.style.display = 'block';
+  //   contactError.style.backgroundColor = 'green';
+  //   contactError.textContent = "Email addressis in lowercase";
+  // };
+  }
